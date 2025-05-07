4433f19ec1f44b236bb81971b936bcc4975acb1b
andreluis98 <andreluis0017@gmail.com>
2025-05-07 18:16:13 -0300

    Feat: Creating and mappead services the api

--- front/gdadc-ui/src/app/app.routes.ts
+++ front/gdadc-ui/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { BookingsComponent } from './pages/booking/bookings/bookings.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        { path: '', component: BookingsComponent },
+];
