6d3bbe18176cc997c81de4c7edaa51f44bb2498f
andreluis98 <andreluis0017@gmail.com>
2025-05-14 15:50:19 -0300

    feat: create and implement a service to fetch the IATA codes of airports.

---
+++ front/gdadc-ui/src/app/core/iata-code/iata-codes.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IataCodesService {
+  private http = inject(HttpClient);
+  airports: any[] = [];
+  
+  getCodesIata(): Observable<any[]> {
+  return this.http.get<any[]>('data/airports.json').pipe(
+    map(data => data.filter(airport => airport.iata_code))
+  );
+}
+}
